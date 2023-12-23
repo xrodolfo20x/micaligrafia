44255166529b842af799e9316eeb374739d3edc0
Juan Manuel Alvarado <54479105+xrodolfo20x@users.noreply.github.com>
2023-12-23 13:25:37 -0500

    Update2 TI.py

--- TI.py
+++ TI.py
@@ -188,4 +188,4 @@ def comparar_imagenes():
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run(app, host='0.0.0.0', port=8000, reload=True, access_log=False)
